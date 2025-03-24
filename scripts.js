c9ad08b0dca48b3c34d8b7b8b67bb557805ef4be
Brian Poon <86692163+Brian-S-Poon@users.noreply.github.com>
2025-03-23 19:35:48 -0700

    Revert "add hamburger toggle"
    This reverts commit f17d8757552435f96587413fa5f739159ee3b4f0.

--- scripts.js
+++ scripts.js
@@ -40,14 +40,3 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
   
-  // Hamburger toggle
-document.addEventListener("DOMContentLoaded", () => {
-  const hamburger = document.getElementById("hamburger");
-  const navLinks = document.getElementById("navLinks");
-
-  if (hamburger && navLinks) {
-    hamburger.addEventListener("click", () => {
-      navLinks.classList.toggle("show");
-    });
-  }
-});
