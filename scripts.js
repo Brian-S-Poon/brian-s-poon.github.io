f17d8757552435f96587413fa5f739159ee3b4f0
Brian Poon <86692163+Brian-S-Poon@users.noreply.github.com>
2025-03-23 19:34:18 -0700

    add hamburger toggle

--- scripts.js
+++ scripts.js
@@ -40,3 +40,14 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
   
+  // Hamburger toggle
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburger = document.getElementById("hamburger");
+  const navLinks = document.getElementById("navLinks");
+
+  if (hamburger && navLinks) {
+    hamburger.addEventListener("click", () => {
+      navLinks.classList.toggle("show");
+    });
+  }
+});
